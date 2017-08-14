caa471a94f0f7bfad4274f43b3344df68d3dbcdc
MEGAYOKOYAN <y.y.as.fog@gmail.com>
2017-08-15 02:45:22 +0900

    first commit

---
+++ Assets/source/BackgroundController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+	private float speed;
+
+	void Update () {
+		transform.Translate (0, -0.02f, 0);
+		if (transform.position.y < -6.5f ) {
+			transform.position = new Vector3 (0, 6.5f, 0);
+		}
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+}
